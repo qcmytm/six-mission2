1d83f567ae843cd1ff76f33f6f2c60f74d8773d0
qcmytm <qcmytm@gmail.com>
2022-06-23 23:55:52 +0800

    3

--- all.js
+++ all.js
@@ -1,21 +1,35 @@
 const swiper = new Swiper('.swiper', {
+  
+    slidesPerView: 3,
+    
+    breakpoints: {
+      /* 螢幕寬度大於等於 992px 時切換為 3 欄 */
+      992: {
+        slidesPerView: 3
+      },
+      /* 螢幕寬度大於等於 768px 時切換為 2 欄 */
+      768: {
+        slidesPerView: 2
+      },
+      /* 更小時都顯示 1 欄 */
+      0: {
+        slidesPerView: 1
+      }
+    },
+
+    spaceBetween: 16,
+
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+
+    
+  
     // Optional parameters
     direction: 'horizontal',
     loop: true,
   
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
   
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
+ 
   });
